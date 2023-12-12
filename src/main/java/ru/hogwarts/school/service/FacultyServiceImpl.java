6c97c5e72edbc49996a59d2b64d2ef09ebeb06c8
Lebedeva-Elena <lebedeva_leb.en@mail.ru>
2023-12-12 21:17:21 +0300

    логирование

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exception.FacultyNotFoundException;
 import ru.hogwarts.school.model.Faculty;
@@ -9,6 +11,7 @@ import java.util.*;
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
+    private final Logger logger = LoggerFactory.getLogger(FacultyServiceImpl.class);
     private final FacultyRepository facultyRepository;
 
     public FacultyServiceImpl(FacultyRepository facultyRepository) {
@@ -17,12 +20,15 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Faculty create(Faculty faculty) {
+        logger.info("Was invoked method for create faculty");
         return facultyRepository.save(faculty);
 
     }
 
     @Override
     public Faculty read(long id) {
+        logger.info("Was invoked method for read faculty");
+        logger.error("There is not faculty with id = " + id);
         return facultyRepository.findById(id).
                 orElseThrow(() -> new FacultyNotFoundException("Факультет с id " + id +
                         " не найден в списке"));
@@ -32,18 +38,21 @@ public class FacultyServiceImpl implements FacultyService {
     @Override
     public Faculty update(Faculty faculty) {
         read(faculty.getId());
+        logger.info("Was invoked method for update faculty");
         return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty delete(long id) {
         Faculty faculty = read(id);
+        logger.info("Was invoked method for delete faculty");
         facultyRepository.delete(faculty);
         return faculty;
     }
 
     @Override
     public Collection<Faculty> readByColor(String color) {
+        logger.info("Was invoked method for readByColor faculty");
         return facultyRepository.findAllByColor(color);
     }
 
